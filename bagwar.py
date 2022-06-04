97b6d88e8f1bd74ede1c7818ff58283b1332a4d7
mikemcsomething <mikemcsomething@gmail.com>
2022-06-03 19:52:52 -0700

    Add files via upload

--- bagwar.py
+++ bagwar.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-def bag_war(reps = 10000):
+#basic bag contents - [1,1,1,1,2,3,3,0,0,00]
+def bag_war(reps = 5000):
 
     small_bag_wins = 0
     big_bag_wins = 0
@@ -10,12 +10,12 @@ def bag_war(reps = 10000):
     #Small Bag   
         
         small_bag_score = 0
-        small_bag_supporters = 4
+        small_bag_supporters = 5
         small_bag_madness = 0
-        original_small_bag_values = [3,3,3,00,0,0,2]
+        original_small_bag_values = [2,3,3,0,0,00,0]
         small_bag_values = original_small_bag_values
         small_bag_audit = []
-        while small_bag_supporters > 1 and small_bag_madness < 4:
+        while small_bag_supporters > 1:
             
             small_bag_pull = np.random.choice(small_bag_values)
         
@@ -29,7 +29,7 @@ def bag_war(reps = 10000):
                 small_bag_supporters -= 2
                 small_bag_madness +=1
             elif small_bag_madness == 4:
-                print("Small Bag Bust!")
+                small_bag_values == original_small_bag_values
             small_bag_score += small_bag_pull
             small_bag_values.remove(small_bag_pull)
             small_bag_audit.append(small_bag_pull) 
@@ -38,13 +38,13 @@ def bag_war(reps = 10000):
     #Big Bag
         
         big_bag_score = 0
-        big_bag_supporters = 4
+        big_bag_supporters = 5
         big_bag_madness = 0
         big_bag_audit = []
-        original_big_bag_values = [1,1,1,3,3,3,0,00,0,2]
+        original_big_bag_values = [1,1,1,1,2,3,3,0,0,00,0]
         big_bag_values = original_big_bag_values
 
-        while big_bag_supporters > 1 and big_bag_madness < 4:
+        while big_bag_supporters > 1:
             
             big_bag_pull = np.random.choice(big_bag_values)
 
@@ -58,7 +58,7 @@ def bag_war(reps = 10000):
                 big_bag_supporters -= 2
                 big_bag_madness +=1
             elif big_bag_madness == 4:
-                print("Big Bag Bust!")
+                big_bag_values == original_big_bag_values
             big_bag_score += big_bag_pull
             big_bag_values.remove(big_bag_pull)  
             big_bag_audit.append(big_bag_pull)
